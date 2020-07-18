19b20ddd7a7a148528a3ab67ae2e9f48ad6855bb
Ben Edwards <benji.edwards60@gmail.com>
2020-07-18 23:09:17 +0100

    Add files via upload

--- Holidays.php
+++ Holidays.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <link href="WebProject.css" rel="stylesheet" type="text/css" />
+    <link rel='shortcut icon' type='image/x-icon' href='favicon.ico' />
     <title>premier choice holidays Holidays Page</title>
 </head>
 
